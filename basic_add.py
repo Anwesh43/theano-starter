24cd0186d0d73d60c1bb233162a65b357b11309b
Anwesh43 <anweshthecool0@gmail.com>
2017-01-05 10:44:32 +0530

    Usage of eval function

--- basic_add.py
+++ basic_add.py
@@ -5,6 +5,7 @@ from theano import pp
 a = T.scalar('a')
 b = T.scalar('b')
 f = a+b
+f2 = a+2*b
 print pp(f)
 print type(a)
 print a.type
@@ -12,3 +13,4 @@ add = theano.function([a,b],f)
 if len(sys.argv) == 3:
     arguments = map(int,sys.argv[1:])
     print add(arguments[0],arguments[1])
+    print f2.eval({a:arguments[0],b:arguments[1]})
