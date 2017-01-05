0738bc749968986ffdd7dcdd8fdbf10bd3d63d6e
Anwesh43 <anweshthecool0@gmail.com>
2017-01-05 10:42:48 +0530

    debugging the steps involved

--- basic_add.py
+++ basic_add.py
@@ -1,9 +1,13 @@
 import theano.tensor as T
 import theano
 import sys
+from theano import pp
 a = T.scalar('a')
 b = T.scalar('b')
 f = a+b
+print pp(f)
+print type(a)
+print a.type
 add = theano.function([a,b],f)
 if len(sys.argv) == 3:
     arguments = map(int,sys.argv[1:])
