976a8e03ff653be1c1661a3de5fc7bd0412436d5
Anwesh43 <anweshthecool0@gmail.com>
2017-01-07 20:42:47 +0530

    seeding random variables

--- random_stream.py
+++ random_stream.py
@@ -4,8 +4,15 @@ from theano.tensor.shared_randomstreams import RandomStreams
 srng = RandomStreams(seed=1000)
 r_uv = srng.uniform((2,2))
 r_nd = srng.normal((2,2))
+rng_val =  r_uv.rng.get_value(borrow=True)
+rng_val.seed(345)
+r_uv.rng.set_value(rng_val,borrow=True)
 r_uniform = function([],r_uv)
 r_normal = function([],r_nd,no_default_updates=True)
+
 print r_uniform()
 print r_normal()
 print r_normal()
+
+
+#printing seeds of random variables
