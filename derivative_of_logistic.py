3c0c1eaf90e2511b6993f216aa88c04a691de614
Anwesh43 <anweshthecool0@gmail.com>
2017-01-08 01:40:17 +0530

    Adding logistic function of matrices

--- derivative_of_logistic.py
+++ derivative_of_logistic.py
@@ -1,7 +1,15 @@
 import theano.tensor as T
 from theano import *
+##For scalar
 x = T.dscalar('x')
 y = 1/(1+T.exp(-x))
 dy = T.grad(y,x)
 grad_softmax = function([x],dy)
 print grad_softmax(10)
+
+#For Matrix
+x_matrix = T.dmatrix('x_matrix')
+y_sum = T.sum(1/(1+T.exp(-x_matrix)))
+dy_matrix = T.grad(y_sum,x_matrix)
+grad_softmax_matrix = function([x_matrix],dy_matrix)
+print grad_softmax_matrix([[1,2],[3,4]])
